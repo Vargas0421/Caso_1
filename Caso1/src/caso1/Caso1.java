617d44d43f187709f8612e92cbf65f93fa13f8af
Vargas0421 <136260759+Vargas0421@users.noreply.github.com>
2023-10-25 20:42:46 -0600

    Add files via upload

---
+++ Caso1/src/caso1/Caso1.java
@@ -0,0 +1,16 @@
+package caso1;
+import javax.swing.JOptionPane;
+public class Caso1 {
+    public static void main(String[] args) {
+        
+        control a = new 
+        // Utilizar los getters para obtener información de la canción
+        System.out.println("ID de la canción: " + cancion1.getIdCancion());
+        System.out.println("Título: " + cancion1.getTitulo());
+        System.out.println("Autor: " + cancion1.getAutor());
+        System.out.println("Duración: " + cancion1.getDuracion() + " minutos");
+        System.out.println("Año de creación: " + cancion1.getAnioCreacion());
+        
+                
+    }
+}
---
+++ Caso1/src/caso1/control.java
@@ -0,0 +1,71 @@
+package caso1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class control {
+
+    private int idCancion;
+    private String titulo;
+    private String autor;
+    private double duracion;
+    private int anioCreacion;
+
+    public control(int idCancion, String autor, int anioCreacion) {
+        this.idCancion = idCancion;
+        this.autor = autor;
+        this.anioCreacion = anioCreacion;
+    }
+
+    public int getIdCancion() {
+        return idCancion;
+    }
+
+    public void setIdCancion(int idCancion) {
+        this.idCancion = idCancion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(double duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getAnioCreacion() {
+        return anioCreacion;
+    }
+
+    public void setAnioCreacion(int anioCreacion) {
+        this.anioCreacion = anioCreacion;
+    }
+
+    
+    
+
+ 
+    
+}
